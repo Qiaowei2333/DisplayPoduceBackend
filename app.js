04d0d1973d831f605600e25b265954fd6931a164
Qiaowei2333 <qxl161930@utdallas.edu>
2019-02-10 21:22:30 -0500

    Add backend api

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+const produce = require('./mockData');
+
+var app = express();
+
+app.get('/produce/:name', function(req, res) {
+    let name = req.params.name;
+    let result = produce.filter((item) => {
+        if(item.name.toLocaleLowerCase().indexOf(name) >=0 ) 
+        return item;
+    });
+    res.send(result);
+})
+
+app.listen(3000);
---
+++ mockData.js
@@ -0,0 +1,105 @@
+
+const produce = [
+    {
+        name: 'produce11',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce22',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce33',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce44',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce55',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce66',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce77',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce88',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce99',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    },
+    {
+        name: 'produce1010',
+        category: 'Food',
+        brand: 'GoodBrand',
+        'price per pound': '1',
+        availability: true,
+        'location in store': {
+            aisle: '2F'
+        }
+    }
+];
+
+module.exports = produce
